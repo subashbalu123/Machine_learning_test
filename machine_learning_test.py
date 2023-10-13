bca8fc4121a8dc47429764540a93bbb732555571
subashbalu07@gmail.com <subashbalu07@gmail.com>
2023-10-13 14:44:12 +0530

    Initial commit

---
+++ machine_learning_test.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.preprocessing import image
+
+# Define constants
+IMAGE_HEIGHT, IMAGE_WIDTH = 224, 224
+NUM_EPOCHS = 10
+
+# Load and prepare data
+georges_df = pd.read_csv('datasets/georges.csv')  # Assuming 'georges.csv' contains file paths and labels
+non_georges_df = pd.read_csv('datasets/non-georges.csv')  # Assuming 'non-georges.csv' contains file paths and labels
+
+# Combine the datasets
+dataset = pd.concat([georges_df, non_georges_df], ignore_index=True)
+
+# Split the dataset into training, validation, and test sets
+train_df, test_df = train_test_split(dataset, test_size=0.2, random_state=42)
+train_df, val_df = train_test_split(train_df, test_size=0.2, random_state=42)
+
+# Define data generators for training, validation, and test sets
+train_data_generator = ImageDataGenerator(
+    rescale=1.0/255.0,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+val_data_generator = ImageDataGenerator(rescale=1.0/255.0)
+
+test_data_generator = ImageDataGenerator(rescale=1.0/255.0)
+
+train_generator = train_data_generator.flow_from_dataframe(
+    dataframe=train_df,
+    x_col='x_col_georges',# Replace with the actual column name for file paths
+    y_col='y_col_georges',# Replace with the actual column name for labels
+    target_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+    batch_size=32,
+    class_mode='binary'
+)
+
+val_generator = val_data_generator.flow_from_dataframe(
+    dataframe=val_df,
+    x_col='x_col_non_georges',
+    y_col='y_col_non_georges',
+    target_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+    batch_size=32,
+    class_mode='binary'
+)
+
+test_generator = test_data_generator.flow_from_dataframe(
+    dataframe=test_df,
+    x_col='x_col_non_georges',
+    y_col='y_col_non_georges',
+    target_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+    batch_size=32,
+    class_mode='binary'
+)
+
+# Build and compile the model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3)))
+model.add(MaxPooling2D(2, 2))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(train_generator, epochs=NUM_EPOCHS, validation_data=val_generator)
+
+# Evaluate the model on the test set
+loss, accuracy = model.evaluate(test_generator)
+print(f"Test loss: {loss}")
+print(f"Test accuracy: {accuracy}")
+
+# Make predictions on a new image
+all_file_paths = georges_df['file_path_column_name'].tolist() + non_georges_df['file_path_column_name'].tolist()
+for img_path in all_file_paths:
+    img = image.load_img(img_path, target_size=(IMAGE_HEIGHT, IMAGE_WIDTH))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0  # Normalize pixel values
+    prediction = model.predict(img_array)
+
+
+    if prediction > 0.5:
+        print("St. George is present in the image.")
+    else:
+        print("St. George is not present in the image.")
